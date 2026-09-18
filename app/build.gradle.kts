f34e39e808fe8d2e9fe5400941fe9fa4f0d599bf
YAMATO <191845361+Yamato2010@users.noreply.github.com>
2026-09-18 14:51:00 +0900

    バージョンアップ 1.0_beta7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "jp.gr.java_conf.SenseMusicClock"
         minSdk = 32
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0_beta6"
+        versionCode = 7
+        versionName = "1.0_beta7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
